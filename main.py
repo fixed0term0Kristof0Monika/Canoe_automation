e0a358a110dbef1e650286c530b31818c8de859d
Kristof Monika <143595895+fixed0term0Kristof0Monika@users.noreply.github.com>
2023-10-16 11:55:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,34 +15,47 @@ import keyboard
 if __name__ == "__main__":
     mycanoe = Canoe()
     mycanoe.open_Canoe()  
-    mycanoe.str_measur()
-    print("Import from file? ")
-    imp_file= input(" Y or N: ").upper()
-         
-    if(imp_file == "Y"):
-        file = openExcelFile()
-        channel = extractFromExcel(file,"B2")
-        chanel_nr = extractFromExcel(file,"B3")
-        message = extractFromExcel(file,"B4")
-        signal = extractFromExcel(file,"B5")
-        value = extractFromExcel(file,"B6")
-        print("Values from Excel: ", channel, chanel_nr, message, signal, value)
-        mycanoe.set_signal(message, signal, value, channel, chanel_nr)
-        print("\tSignal after change")
-        mycanoe.get_signal(message, signal, channel, chanel_nr)
-    else: 
-        mycanoe.set_signal("EngineState", "EngineSpeed", 1234)
-        print("\tSignal after change")
-        mycanoe.get_signal("EngineState", "EngineSpeed",)
-    #system variables    
-    print("Operating with system variables")
-    mycanoe.set_syst_signal("Engine::EngineSpeedDspMeter", 20)
-    mycanoe.get_syst_signal("Engine::EngineSpeedDspMeter")
+    # mycanoe.str_measur()
     
-    #handle TestModes
-    print("Start/Stop test cases")
-    mycanoe.send_diagnostic("Door", "DefaultSession_Start", False)
-    mycanoe.send_diagnostic("Door", '10 01')
+    # print("Import from file? ")
+    # imp_file= input(" Y or N: ").upper()
+         
+    # if(imp_file == "Y"):
+    #     file = openExcelFile()
+    #     channel = extractFromExcel(file,"B2")
+    #     chanel_nr = extractFromExcel(file,"B3")
+    #     message = extractFromExcel(file,"B4")
+    #     signal = extractFromExcel(file,"B5")
+    #     value = extractFromExcel(file,"B6")
+    #     print("Values from Excel: ", channel, chanel_nr, message, signal, value)
+    #     mycanoe.set_signal(message, signal, value, channel, chanel_nr)
+    #     print("\tSignal after change")
+    #     mycanoe.get_signal(message, signal, channel, chanel_nr)
+    # else: 
+    
+    # mycanoe.set_signal("EngineState", "EngineSpeed", 1234)
+    # print("\tSignal after change")
+    # mycanoe.get_signal("EngineState", "EngineSpeed",)
+    # #system variables    
+    # print("Operating with system variables")
+    # mycanoe.set_syst_signal("Engine::EngineSpeedDspMeter", 20)
+    # mycanoe.get_syst_signal("Engine::EngineSpeedDspMeter")
+    
+    # #handle TestModes
+    # print("Start/Stop test cases")
+    # mycanoe.send_diagnostic("Door", "DefaultSession_Start", False)
+    # mycanoe.send_diagnostic("Door", '10 02')
+    
+    # handle environment variables
+    print("Change environment variables")
+    variable = "Env_DoipConnectVeh"
+    print(mycanoe.get_EnvVar(variable))
+    
+    #connectivity with ethernet
+    mycanoe.Connect_DOIP(variable)
+    print("Now REad_DTC")
+    mycanoe.Read_DTC()
+    
     
     print("Press s to stop measurements and quit")
         #press key to stop measurement \ close application
--- open_canoeobj.py
+++ open_canoeobj.py
@@ -3,7 +3,8 @@ from py_canoe import CANoe
 from time import sleep as wait
 import tkinter.filedialog
 import tkinter as tk
-import win32com.client
+from win32com.client import *
+from win32com.client.connect import *
  
 #implementing a class which has the following modules:
 #open CANoe by user input of the specific configuration file
@@ -19,10 +20,7 @@ class Canoe:
     
     def __init__(self):
         self.canoe_inst = CANoe()
-        
-    def printsomething(self):
-        return "blabla"
-        
+                
     # module for opening CANoe
     def open_Canoe(self):
         root = tk.Tk()
@@ -61,19 +59,79 @@ class Canoe:
             self.canoe_inst.set_system_variable_value(sys_var_name, value)
         except:
             if(not isinstance(sys_var_name, str) ):
-                print("Wrong system variable namespace or name, \ntry Engine or Lights insted with its name eg. EngineSpeedEntry")
+                print("Wrong system variable namespace or name")
             elif(not isinstance(value, int)):
                 print("Value is not a valid integer!")
             
     def get_syst_signal(self, sys_var_name):
         try:
-            self.canoe_inst.get_system_variable_value(sys_var_name)
+           return self.canoe_inst.get_system_variable_value(sys_var_name)
         except:
              if(not isinstance(sys_var_name, str)):
-                print("Wrong system variable namespace or name, \ntry Engine or Lights insted with its name eg. EngineSpeedEntry")
+                print("Wrong system variable namespace or name,")
     
     def send_diagnostic(self,diag_ecu_qualifier_name , request, request_in_bytes = True):
-        self.canoe_inst.send_diag_request(diag_ecu_qualifier_name, request, request_in_bytes)
+        return self.canoe_inst.send_diag_request(diag_ecu_qualifier_name, request, request_in_bytes)
+    
+    # Environment variables manipulation
+    def get_EnvVar(self, var):
+        try:
+            return self.canoe_inst._CANoe__canoe_objects.get("Environment").GetVariable(var).Value
+        except ValueError:
+            print ("Couldn't find value")
+
+    def set_EnvVar(self, var, value):
+        # set the environment varible
+        result = self.canoe_inst._CANoe__canoe_objects.get("Environment").GetVariable(var)
+        result.Value = value
+        wait(1)
+        if( self.get_EnvVar(var) == value):
+            print("Set to: ", self.get_EnvVar(var))
+        else:
+            raise ValueError("Value not corresponding")
+            
+    #Connectivity with ethernet 
+    def Connect_DOIP(self, var):
+        try:
+            self.set_EnvVar(var, 1)
+            wait(2)
+            self.set_EnvVar(var, 0)   
+        except AttributeError:
+            print("Wrong value or Environment variable")
+            
+    # Reading DTC   
+    def Read_DTC(self):
+        try:
+            self.set_EnvVar("Env_DoipConnectVeh", 1)
+            wait(2)
+            self.set_EnvVar("Env_DoipConnectVeh", 0)   
+        except AttributeError:
+            print("Wrong value or Environment variable")    
+        rec= self.get_EnvVar("Env_DoipDirectReceive")   
+        # put the received Diagnostic code into a list
+        listOfDiag = splitDiagResponse(rec)
+        print(listOfDiag)
+        
+    #RBEOL
+    def RBEOL(self):
+        try:
+            self.set_EnvVar("Env_RBEOL", 1)
+            wait(2)
+            self.set_EnvVar("Env_RBEOL", 0)   
+        except AttributeError:
+            print("Wrong value or Environment variable")
+        
+    #RBEOL_unlock
+    def RBEOL_unlock(self):
+        try:
+            self.set_EnvVar("Env_JLR_Prog_Unlock", 1)
+            wait(2)
+            self.set_EnvVar("Env_JLR_Prog_Unlock", 0)   
+        except AttributeError:
+            print("Wrong value or Environment variable")
+     
+            
+        
     #stop measurements
     def stop_measur(self):
         wait(5)
@@ -84,3 +142,31 @@ class Canoe:
         wait(5)
         self.canoe_inst.quit()
         
+
+def splitDiagResponse(str_var):
+    usefull_code = str_var[6:]
+    firstSix = str_var[:6]
+    
+    if firstSix != "59027B":
+        print("Error")
+    else:
+        if len(str_var)==6:
+            print("No DTC")
+            
+        elif len(str_var)>6:
+            mylist = []
+            for i in range(0, len(usefull_code), 8):
+                mylist.append(usefull_code[i:i+8])
+
+            dict_code = []
+            for i in mylist:
+                dict_code.append(i[:6])
+                dict_code.append(i[6:])
+                
+            final_list=[]
+            for i in range(0, len(dict_code), 2):
+                final_list.append(dict_code[i:i+2])
+            
+            return final_list  
+            
+    
